9a70691df4d1ae7071d79d02612f858360f8b44d
findingFireBane <sizwe95@hotmail.com>
2026-04-03 15:10:52 -0400

    Added: user model Added UserDAO Altered:VehicleDAO to format methods to make it easier to clean

---
+++ v1/src/main/java/app/model/User.java
@@ -0,0 +1,36 @@
+package app.model;
+
+public class User {
+
+    private int id;
+    private String username;
+    private String email;
+    private int role_id;
+
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public int getRole_id() {
+        return role_id;
+    }
+    public void setRole_id(int role_id) {
+        this.role_id = role_id;
+    }
+
+}
---
+++ v1/src/main/java/app/repository/UserDAO.java
@@ -0,0 +1,16 @@
+package app.repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import app.model.Vehicle;
+import app.util.DatabaseConnection;
+
+public class UserDAO {
+
+    public 
+
+}
